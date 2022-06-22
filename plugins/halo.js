ebc5b8e8eb3b79c3666b1f963d1a25b7b4e29695
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-06-22 11:59:33 +0700

    Update halo.js

--- plugins/halo.js
+++ plugins/halo.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/377aae9aa4e207e115721.jpg', m, { packname: "sticker by", author: "FachriBot-MD" })
+conn.sendImageAsSticker(m.chat, 'https://raw.githubusercontent.com/Ipulpachri/fachribotzv3/FachriBotzv3/sticker/Hallohaori.webp', m, { packname: "sticker by", author: "FachriBot-MD" })
 }
 
 handler.customPrefix = /^(halo)$/i
