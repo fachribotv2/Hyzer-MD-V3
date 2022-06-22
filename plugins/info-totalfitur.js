70f4aef45bcd773743b379a3ab9a00ac134d5615
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-06-22 12:53:33 +0700

    Update info-totalfitur.js

--- plugins/info-totalfitur.js
+++ plugins/info-totalfitur.js
@@ -1,5 +1,9 @@
+
 let handler = async (m, { conn }) => {
+let totalf = Object.values(global.plugins).filter(
 let ye = `@${m.sender.split`@`[0]}`
+    (v) => v.help && v.tags
+  ).length;
 let esce = `
 Hai ${ye}, 
 Total Fitur saat ini: ${totalf}
@@ -8,6 +12,5 @@ conn.sendBut(m.chat, esce, wm3, 'Oke', 'ok', m)
 handler.help = ['totalfitur']
 handler.tags = ['info']
 handler.command = ['totalfitur']
-
 module.exports = handler
 
