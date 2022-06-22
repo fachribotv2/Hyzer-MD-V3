2560b681d3a965e829eb37dab5b22efc4194f18a
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-06-22 12:04:37 +0700

    Create p.js

---
+++ plugins/p.js
@@ -0,0 +1,9 @@
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+
+conn.sendImageAsSticker(m.chat, 'https://raw.githubusercontent.com/Ipulpachri/fachribotzv3/FachriBotzv3/sticker/P.webp', m, { packname: "Gausah P" anjir 🗿", author: "FachriBot-MD" })
+}
+
+handler.customPrefix = /^(p)$/i
+handler.command = new RegExp
+
+module.exports = handler
