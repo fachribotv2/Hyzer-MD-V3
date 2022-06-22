5a2b2572f2730de5278ec21537ef057ca11f276d
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-06-22 17:22:28 +0700

    Update main-donasi.js

--- plugins/main-donasi.js
+++ plugins/main-donasi.js
@@ -1,11 +1,12 @@
 let fs = require('fs')
 let handler = async (m, { conn }) => {
 let teks = 'donasi'
+let ye = `@${m.sender.split`@`[0]}`
 let dana = global.dana
 let pulsa = global.pulsa
 let gopay = global.gopay
 let numberowner = global.numberowner
-let anu = `Hai 👋
+let anu = `Hai Kak, ${ye}👋
 Kalian bisa mendukung saya agar bot ini tetap up to date dengan:
 ┌〔 Donasi • Emoney 〕
 ├ Dana : ${dana}
@@ -14,12 +15,10 @@ Kalian bisa mendukung saya agar bot ini tetap up to date dengan:
 └────
 Berapapun donasi kalian akan sangat berarti 👍
 
-Arigatou!
-
 Contact person Owner:
-wa.me/${numberowner} (Owner)
+wa.me/${numberowner} (Fachri)
 
-*donasi via follow ig juga boleh*`
+*Donasi via follow ig juga boleh*`
   conn.sendButtonImg(m.chat, fla + teks, anu, instagram, 'Back', '.help', m) 
 }
 handler.help = ['donasi', 'donate']
