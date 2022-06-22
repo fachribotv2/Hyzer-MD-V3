909a8958b76e3f09cc5b66fb3aeedee9a7ecc51e
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-06-22 12:41:12 +0700

    Update info-totalfitur.js

--- plugins/info-totalfitur.js
+++ plugins/info-totalfitur.js
@@ -3,12 +3,12 @@ let handler = async (m, { conn, args, command }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
- await conn.sendButton(m.chat, `Total Fitur Bot Saat ini: ${totalf}\n`,wm + '\n\n' + wm3, giflogo, [['MENU','.menu']], m, {
+ await conn.sendButton(m.chat, `Total Fitur Bot Saat ini: ${totalf}\n`,wm2 + '\n\n' + wm3, giflogo, [['MENU','.menu']], m, {
 contextInfo: { externalAdReply :{
                         mediaUrl: '',
                         mediaType: 2,
                         description: 'anu',
-                        title: bottime,
+                        title: wm,
                         body: 'Total Cintaku Padamu ><',          previewType: 0,
                         thumbnail: fs.readFileSync("./d11e20d44501e1a59439b5344e07f5d7.jpg"),
                         sourceUrl: instagram
