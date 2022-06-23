860c837da262f7a0d2a0bb1aa6ec8f3a9554e7bf
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-06-23 07:45:58 +0700

    Update tes123.js

--- plugins/tes123.js
+++ plugins/tes123.js
@@ -43,7 +43,7 @@ let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.documen
             ]
         }
         return await conn.sendMessage(m.chat, message)*/
-conn.reply(m.chat, info, m, { contextInfo: { externalAdReply: {title: global.wm, body: 'Yaw? ada apa kak?', sourceUrl: snh, thumbnail: fs.readFileSync('./thumbnail.jpg')  }}})
+conn.reply(m.chat, info, m, { contextInfo: { externalAdReply: {title: global.wm3, body: 'Yaw? ada apa kak?', sourceUrl: instagram, thumbnail: fs.readFileSync('./d11e20d44501e1a59439b5344e07f5d7.jpg')  }}})
 }
 handler.customPrefix = /^(tes|tess|test)$/i
 handler.command = new RegExp
