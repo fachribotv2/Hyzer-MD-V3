061a56e1983d1d341e5215762719c97ecef82c90
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-06-23 07:57:30 +0700

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -1,8 +1,35 @@
-let handler = async(m, { conn }) => {
-conn.sendContact(m.chat, numberowner, nameowner, m)
-}
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let handler = async (m, {conn}) => {
+const vcard = `BEGIN:VCARD
+VERSION:3.0
+N:Sy;Bot;;;
+FN: Elyas
+item.ORG: Fachri
+item1.TEL;waid=6285713041886:6285713041886@s.whatsapp.net
+item1.X-ABLabel:No Owner FachriBot
+item2.EMAIL;type=INTERNET:saefulfachri18@gmail.com
+item2.X-ABLabel:Email
+item3.ADR:;;🇮🇩INDONESIA;;;;
+item3.X-ABADR:ac
+item3.X-ABLabel:📍 LOCATION
+item4.EMAIL;type=INTERNET:Seseorang Akan Selalu Terlihat Buruk di Mata Pembenci!
+item4.X-ABLabel:somebody 👤
+item5.URL:${instagram}
+item5.X-ABLabel:Website
+END:VCARD`
+const sentMsg  = await conn.sendMessage(
+    m.chat,
+    { 
+        contacts: { 
+            displayName: 'OWNER GW', 
+            contacts: [{ vcard }] 
+        }
+    }
+)
+await conn.reply(m.chat, "itu Nomor Orang Ganteng", sentMsg)}
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
+
 handler.command = /^(owner|creator)$/i
 
 module.exports = handler
