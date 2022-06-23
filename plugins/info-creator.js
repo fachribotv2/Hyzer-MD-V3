979a545f4b627a3df5f37d6050539221773fde1d
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-06-23 09:47:19 +0700

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -15,31 +15,12 @@ item3.X-ABLabel:📍 LOCATION
 item4.URL:${instagram}
 item4.X-ABLabel:Website
 END:VCARD`
-const vcard2 = `BEGIN:VCARD
-VERSION:3.0
-N:Sy;Bot;;;
-FN: Romli 
-item.ORG: Seseorang Akan Selalu Terlihat Buruk di Mata Pembenci!
-item1.TEL;waid=6281322667077:6281322667077@s.whatsapp.net
-item1.X-ABLabel:Sibuk 
-item2.EMAIL;type=INTERNET:saefulfachri18@gmail.com
-item2.X-ABLabel:Email
-item3.ADR:;;🇮🇩 INDONESIA;;;;
-item3.X-ABADR:ac
-item3.X-ABLabel:📍 LOCATION
-item4.URL:${instagram}
-item4.X-ABLabel:Website
-END:VCARD`
 const sentMsg  = await conn.sendMessage(
     m.chat,
     { 
         contacts: { 
             displayName: 'OWNER GW', 
-            contacts: [{ vcard }] 
-    {
-        contacts: { 
-            displayName: 'OWNER GW', 
-            contacts: [{ vcard2 }] 
+            contacts: [{ vcard }]  
         }
     }
 )
