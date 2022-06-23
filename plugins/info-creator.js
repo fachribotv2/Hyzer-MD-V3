fa01ebb3f319c25c52f5b205a2dba67a4179ed8e
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-06-23 08:06:59 +0700

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -3,19 +3,17 @@ let handler = async (m, {conn}) => {
 const vcard = `BEGIN:VCARD
 VERSION:3.0
 N:Sy;Bot;;;
-FN: Elyas
-item.ORG: Fachri
+FN: Fachri 
+item.ORG: Seseorang Akan Selalu Terlihat Buruk di Mata Pembenci!
 item1.TEL;waid=6285713041886:6285713041886@s.whatsapp.net
-item1.X-ABLabel:No Owner FachriBot
+item1.X-ABLabel:Sibuk 
 item2.EMAIL;type=INTERNET:saefulfachri18@gmail.com
 item2.X-ABLabel:Email
 item3.ADR:;;🇮🇩INDONESIA;;;;
 item3.X-ABADR:ac
 item3.X-ABLabel:📍 LOCATION
-item4.EMAIL;type=INTERNET:Seseorang Akan Selalu Terlihat Buruk di Mata Pembenci!
-item4.X-ABLabel:somebody 👤
-item5.URL:${instagram}
-item5.X-ABLabel:Website
+item4.URL:${instagram}
+item4.X-ABLabel:Website
 END:VCARD`
 const sentMsg  = await conn.sendMessage(
     m.chat,
