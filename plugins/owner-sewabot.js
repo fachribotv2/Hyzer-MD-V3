6ade3f9d9a541000e36cc6a1a5d5cab794423893
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-06-30 21:05:04 +0700

    Update owner-sewabot.js

--- plugins/owner-sewabot.js
+++ plugins/owner-sewabot.js
@@ -3,23 +3,24 @@ let teks = 'Sewa FachriBot'
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
-    fkontak = {
-	"key": {
-    "participants":"0@s.whatsapp.net",
-		"remoteJid": "status@broadcast",
-		"fromMe": false,
-		"id": "Halo"
-	},
-	"message": {
-		"contactMessage": {
-			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-		}
-	},
-	"participant": "0@s.whatsapp.net" 
+    ftroli = {
+    key : {
+    remoteJid: 'status@broadcast',
+    participant : '0@s.whatsapp.net'
+    },
+    message: {
+    orderMessage: {
+    itemCount : 2022,
+    status: 1,
+    surface : 1,
+    message: `Hai Kak ${name}!`, 
+    orderTitle: `Sewa ▸`,
+    thumbnail: await (await fetch(fla + 'Sewa')).buffer(), //Gambarnye
+    sellerJid: '0@s.whatsapp.net' 
                             }
                           }
                         }
-conn.send2ButtonImg(m.chat, fla + teks, price1, 'Mau sewa bot?', 'OWNER', '#owner', 'DONASI', '#donasi', fkontak)
+conn.send2ButtonImg(m.chat, fla + teks, price1, 'Mau sewa bot?', 'OWNER', '#owner', 'DONASI', '#donasi', ftroli)
 }
 handler.help = ['sewa', 'sewabot']
 handler.tags = ['info']
