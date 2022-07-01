f9c46b39289671eec00b50102061bd199629f1de
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-07-01 12:54:01 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -11,8 +11,8 @@ let ye = `@${m.sender.split`@`[0]}`
 let esce = `
 Hai ${ye} Lagi Nyari Sc Ya? 
 
-Nih Link Sc Nya Dibawah :
-https://github.com/Ipulpachri/Hyzer-MD-V3
+Sc *FachriBot-MD :*
+Private ~
 `
 fdoc = {
    key : {
@@ -25,7 +25,36 @@ fdoc = {
                            }
                           }
                         }
-conn.sendBut(m.chat, esce, 'Jgn Lupa Kasih Star&Follow Ya kak', 'Oke', 'thanks', fdoc) 
+const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           hydratedContentText: esce,
+           locationMessage: { 
+           jpegThumbnail: { 
+           hydratedFooterText: wm,
+           hydratedButtons: [{
+             urlButton: {
+               displayText: 'My Github',
+               url: 'github.com/Ipulpachri'
+             }
+
+           },
+               {
+             quickReplyButton: {
+               displayText: 'Back',
+               id: '.menu',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
 }
 handler.help = ['sc', 'sourcecode']
 handler.tags = ['info']
