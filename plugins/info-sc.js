8544684b22fc1fd7ee0988cbdefe1c5fce69c50c
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-07-04 19:35:38 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,11 +1,17 @@
 
 let handler = async (m, { conn }) => {
 let ye = `@${m.sender.split`@`[0]}`
-let esce = `
-❏━═┅═━–––
+let esce = `❏━═┅═━–––
 │⬡ *SCRIPT:* Private🔒
 │⬡ *BASE:* https://github.com/Hyzerr/Hyzer-MD-V3
-❑━═┅═━––––––`
+❑━═┅═━––––––
+
+📍 *N o t e :* 
+★ Jangan lupa minta izin owner sebelum menggunakan scriptnya kak!
+★ Jangan Lupa kasih star, follow & kasih credit
+★ Dilarang menjual Script Ini!
+★ Jika menemukan bug di script, harap lapor owner
+★ Dilarang reupload sc, Hanya boleh fork`
 fdoc = {
    key : {
    remoteJid: 'status@broadcast',
