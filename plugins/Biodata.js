0a902fe12216eb739e4b0781e7373986542f7d0e
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-07-04 17:55:57 +0700

    Update Biodata.js

--- plugins/Biodata.js
+++ plugins/Biodata.js
@@ -4,23 +4,24 @@ let teks = 'Fachri'
 let ext = `
 *BIODATA OWNER*
 
-*Nama* : Fachri
-*Umur* : 14
-*Alamat* : Pemalang, Jawa tengah, Indonesia
-*Kelas* : 2 smp
-*Status* : Private
+⬡ *Nama* : Fachri
+⬡ *Umur* : 14
+⬡ *TTL* : 17.Okto.2k8
+⬡ *Alamat* : Pemalang, Jawa tengah, Indonesia
+⬡ *Kelas* : 2 smp
+⬡ *Status* : Private
 
 *SOSIAL MEDIA*
 
-*instagram* : @sfdesign.id
-*Facebook* : @Saeful Fachri
-*Youtube* : SF Design
-*Email* : saefulfachri18@gmail.com
+⬡ *instagram* : @sfdesign.id
+⬡ *Facebook* : @Saeful Fachri
+⬡ *Youtube* : SF Design
+⬡ *Email* : saefulfachri18@gmail.com
  
 _Oke udah itu aja terimakasih_
 
 `.trim()
-conn.send3ButtonImg(m.chat, fla + teks, ext, 'Recoded By Fachri', 'Nomor Owner', '.owner', 'Donasi', '.donasi', 'Sewa Bot', '.sewabot', m)
+conn.send3ButtonLoc(m.chat, await (await fetch(image)).buffer(), ext, 'Recoded By Fachri', 'Nomor Owner', '.owner', 'Donasi', '.donasi', 'Sewa Bot', '.sewabot', m)
 
 }
 handler.help = ['owner']
