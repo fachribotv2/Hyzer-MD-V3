7b17c1299e710c3df6b663a04b1bd6c18279d943
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-07-05 17:34:27 +0700

    Update info-totalfitur.js

--- plugins/info-totalfitur.js
+++ plugins/info-totalfitur.js
@@ -3,11 +3,21 @@ let handler = async (m, { conn }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
+    fdoc = {
+   key : {
+   remoteJid: 'status@broadcast',
+   participant : '0@s.whatsapp.net'
+   },
+   message: {
+   documentMessage: {
+   title: wm, 
+                            }
+                          }
+                        }
 let ye = `@${m.sender.split`@`[0]}`
-let esce = `
-Hai ${ye}, 
-Total Fitur saat ini: ${totalf}`
-conn.sendBut(m.chat, esce, wm3, 'Owner', '.owner', m) 
+let esce = ` 
+Total Fitur FachriBot: ${totalf}`
+conn.sendBut(m.chat, esce, 'Hai Kak, ${ye}', 'Owner', '.owner', fdoc) 
 }
 handler.help = ['totalfitur']
 handler.tags = ['info']
