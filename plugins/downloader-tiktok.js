8a09d5a48eebf59fa32b7d3a8473da1b0eb363ff
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-07-06 06:51:34 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,9 +1,20 @@
 const hxz = require("hxz-api")
 let handler = async(m,{text, conn}) => {
+    fdoc = {
+   key : {
+   remoteJid: 'status@broadcast',
+   participant : '0@s.whatsapp.net'
+   },
+   message: {
+   documentMessage: {
+   title: wm, 
+                            }
+                          }
+                        }
 
 let p = await  hxz.ttdownloader(text)
 const { nowm, wm, audio } = p
-conn.sendFile(m.chat, nowm, null, 'Nih', m)
+conn.sendFile(m.chat, nowm, null, 'Nih Kak', fdoc)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
