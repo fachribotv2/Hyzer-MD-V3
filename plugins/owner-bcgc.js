509b9f561a8bc4896556352d7124777b08ce7503
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-07-05 12:26:10 +0700

    Update owner-bcgc.js

--- plugins/owner-bcgc.js
+++ plugins/owner-bcgc.js
@@ -1,4 +1,20 @@
 let handler = async (m, { conn, isROwner, text }) => {
+const fgc = {
+	"key": {
+		"fromMe": false,
+		"participant": "0@s.whatsapp.net",
+		"remoteJid": "0@s.whatsapp.net"
+	},
+	"message": {
+		"groupInviteMessage": {
+			"groupJid": "6285240750713-1610340626@g.us",
+			"inviteCode": "mememteeeekkeke",
+			"groupName": "Grub Bot", 
+            "caption": "「 Fachri Bot Broadcast 」", 
+            'jpegThumbnail': fs.readFileSync('./d11e20d44501e1a59439b5344e07f5d7.jpg')
+		}
+	}
+}
     const delay = time => new Promise(res => setTimeout(res, time))
     let getGroups = await conn.groupFetchAllParticipating()
     let groups = Object.entries(getGroups).slice(0).map(entry => entry[1])
@@ -9,7 +25,7 @@ let handler = async (m, { conn, isROwner, text }) => {
     m.reply(`Mengirim Broadcast Ke ${anu.length} Chat, Waktu Selesai ${anu.length * 0.5 } detik`)
     for (let i of anu) {
     await delay(500)
-    conn.send2ButtonImg(i, fbm, `${pesan}`, wm, 'OWNER', '.owner', 'MENU', '.menu', null).catch(_ => _)
+    conn.send2ButtonImg(i, fbm, `${pesan}`, wm, 'OWNER', '.owner', 'MENU', '.menu', fgc).catch(_ => _)
     }
   m.reply(`Sukses Mengirim Broadcast Ke ${anu.length} Group`)
 }
