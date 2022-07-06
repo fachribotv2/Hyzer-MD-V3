315db7d61b94418dc0728dd294cb000692105f27
Fachriv2 <108655134+fachribotv2@users.noreply.github.com>
2022-07-06 16:42:46 +0700

    Create Owner-broadcastchat.js

---
+++ plugins/Owner-broadcastchat.js
@@ -0,0 +1,20 @@
+import { randomBytes } from 'crypto'
+
+let handler = async (m, { conn, text }) => {
+  let chats = Object.entries(conn.chats).filter(([jid, chat]) => !jid.endsWith('@g.us') && chat.isChats).map(v => v[0])
+  let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
+  let teks = text ? text : cc.text
+  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_`, m)
+  for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 All Chat Broadcast 」\n', true).catch(_ => _)
+  m.reply('Selesai Broadcast All Chat :)')
+}
+handler.help = ['bcchats <teks>'].map(v => v + ' <teks>')
+handler.tags = ['owner']
+handler.command = /^(bcchats)$/i
+
+handler.owner = true
+
+export default handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
