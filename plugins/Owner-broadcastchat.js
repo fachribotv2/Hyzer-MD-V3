56856dacfcec06c9a0c8e6bcc01fc731c2ce9d15
Fachriv2 <108655134+fachribotv2@users.noreply.github.com>
2022-07-06 17:00:18 +0700

    Update Owner-broadcastchat.js

--- plugins/Owner-broadcastchat.js
+++ plugins/Owner-broadcastchat.js
@@ -8,7 +8,7 @@ let fetch = require('node-fetch')
 let handler  = async (m, { conn, text }) => {
   let time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss')
   let thumb = 'https://telegra.ph/file/af6386959c16abe3c2bb0.jpg'
-  let chats = conn.chats.all().filter(v => !v.read_only && v.message).map(v => v.jid)
+  let chats = Object.entries(conn.chats).filter(([jid, chat]) => !jid.endsWith('@g.us') && chat.isChats).map(v => v[0])
   let content = await conn.cMod(m.chat, m, /bc|broadcast/i.test(text) ? text : text )
   for (let id of chats) /*conn.send2ButtonLoc*/conn.send2Button(id, `${text}`.trim(), `\n_*ALL BROADCAST*_\n${time}`, 'Owner', '.owner', 'Menu', '.menu', /*'Donasi', '.ds'*/)
   conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_`, m)
