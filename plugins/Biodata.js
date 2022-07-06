eb8e707520760cc69c233ac985205bc0b5d97bb4
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-07-06 10:53:31 +0700

    Update Biodata.js

--- plugins/Biodata.js
+++ plugins/Biodata.js
@@ -1,6 +1,7 @@
 let fs = require('fs')
 let fetch = require('node-fetch')
 let handler = async (m, { conn, text }) => {
+let ppown = await conn.profilePictureUrl(owner + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teks = 'Fachri'
 let ext = `
 *BIODATA OWNER*
@@ -14,15 +15,35 @@ let ext = `
 
 *SOSIAL MEDIA*
 
+⬡ *whatsapp* : wa.me/6285713041886
 ⬡ *instagram* : @sfdesign.id
 ⬡ *Facebook* : @Saeful Fachri
 ⬡ *Youtube* : SF Design
 ⬡ *Email* : saefulfachri18@gmail.com
+
+*SKILLS*
+
+> JavaScript [89.7%]
+> Python [13.4%]
+> CSS [5.1%]
+> Html [1.2%]
  
+
 _Oke udah itu aja terimakasih_
 
 `.trim()
-conn.send3ButtonImg(m.chat, fla + teks, ext, 'Recoded By Fachri', 'Nomor Owner', '.owner', 'Donasi', '.donasi', 'Sewa Bot', '.sewabot', m)
+fdoc = {
+   key : {
+   remoteJid: 'status@broadcast',
+   participant : '0@s.whatsapp.net'
+   },
+   message: {
+   documentMessage: {
+   title: wm, 
+                            }
+                          }
+                        }    
+conn.send3ButtonImg(m.chat, ppown, ext, '© Recoded By Fachri', 'Nomor Owner', '.owner', 'Donasi', '.donasi', 'Sewa Bot', '.sewabot', fdoc)
 
 }
 handler.help = ['owner']
