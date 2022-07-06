b5c060732a6970a937f08d679df081bd0bfe00ca
Fachriv2 <108655134+fachribotv2@users.noreply.github.com>
2022-07-06 13:59:24 +0700

    Update downloader-ytv.js

--- plugins/downloader-ytv.js
+++ plugins/downloader-ytv.js
@@ -2,6 +2,7 @@ let limit = 1024
 let fetch = require('node-fetch')
 const { servers, ytv } = require('../lib/y2mate')
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
+let fkontak = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': fs.readFileSync('./d11e20d44501e1a59439b5344e07f5d7.jpg'), thumbnail: fs.readFileSync('./d11e20d44501e1a59439b5344e07f5d7.jpg'),sendEphemeral: true}}} 
   if (!args || !args[0]) throw 'Uhm... urlnya mana?'
   let chat = global.db.data.chats[m.chat]
   let server = (args[1] || servers[0]).toLowerCase()
@@ -9,7 +10,7 @@ let handler = async (m, { conn, args, isPrems, isOwner }) => {
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   let name = await conn.getName(m.sender)
 let wm = global.wm
-  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
+  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', fkontak)
 *Title:* ${title}
 *Filesize:* ${filesizeF}
 *${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
@@ -17,7 +18,7 @@ let wm = global.wm
   let _thumb = {}
   try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
   catch (e) { }
-  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp4', `
+  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp4', fkontak)
 *Title:* ${title}
 *Filesize:* ${filesizeF}
 `.trim(), m, false, {
