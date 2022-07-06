7ff335da880c68797a796d5cb93b3b934d64f985
Fachriv2 <108655134+fachribotv2@users.noreply.github.com>
2022-07-06 12:37:16 +0700

    Update main-donasi.js

--- plugins/main-donasi.js
+++ plugins/main-donasi.js
@@ -17,7 +17,23 @@ Berapapun donasi kalian akan sangat berarti 👍
 *Contact person Owner:*
 wa.me/${numberowner} (Fachri)
 `
-  conn.send3ButtonImg(m.chat, fla + teks, anu, wm, 'Owner', '.owner', 'Info Owner', '.infoowner', 'Sewa', '.sewabot', m) 
+const fgc = {
+	"key": {
+		"fromMe": false,
+		"participant": "0@s.whatsapp.net",
+		"remoteJid": "0@s.whatsapp.net"
+	},
+	"message": {
+		"groupInviteMessage": {
+			"groupJid": "6285240750713-1610340626@g.us",
+			"inviteCode": "mememteeeekkeke",
+			"groupName": "Grub Bot", 
+            "caption": "Donasi Bg Biar Botnya On 24 jm", 
+            'jpegThumbnail': fs.readFileSync('./d11e20d44501e1a59439b5344e07f5d7.jpg')
+		}
+	}
+}
+  conn.send3ButtonImg(m.chat, fla + teks, anu, wm, 'Owner', '.owner', 'Info Owner', '.infoowner', 'Sewa', '.sewabot', fgc) 
 }
 handler.help = ['donasi', 'donate']
 handler.tags = ['xp', 'info']
